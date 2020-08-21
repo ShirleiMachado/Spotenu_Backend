f9de153d3478b7f973321bbbb20f9c3f352ef6ef
Shirlei-Machado <schiufreitas@gmil.com>
2020-08-21 12:59:00 -0300

    signup, signupAdmin, Login

---
+++ src/data/UserDatabase.ts
@@ -0,0 +1,60 @@
+import { BaseDataBase } from "./BaseDatabase";
+import { User } from "../model/User";
+
+
+export class UserDatabase extends BaseDataBase {
+  public static TABLE_NAME = "USERS";
+
+  public async CreateUser(
+    id: string,
+    name: string,
+    nickname:string,
+    email: string,
+    password: string,
+    role: string
+  ): Promise<void> {
+    await this.getconnection()
+      .insert({
+        id,
+        name,
+        nickname,
+        email,
+        password,
+        role
+      })
+      .into(UserDatabase.TABLE_NAME);
+  }
+
+  public async getUserNickname(nickname: string): Promise<User> {
+    const result = await this.getconnection()
+      .select("*")
+      .from(UserDatabase.TABLE_NAME)
+      .where({ nickname});
+    const data = result[0];
+    const user = new User(data.id, data.name, data.nickname, data.email, data.password, data.role);
+    return user;
+  }
+
+  public async getPassword(password: string): Promise<any> {
+    const result = await this.getconnection()
+    .select("*")
+    .from(UserDatabase.TABLE_NAME)
+    .where({ password });
+
+    return result[0];
+  }
+
+  public async getUserById(id: string): Promise<any> {
+    const result = await this.getconnection()
+      .select("*")
+      .from(UserDatabase.TABLE_NAME)
+      .where({ id });
+
+    return result[0];
+  }
+
+  public async deleteUser(id: string): Promise<void> {
+    await this.getconnection().raw(`
+    DELETE FROM ${UserDatabase.TABLE_NAME} WHERE id = "${id}"`);
+  }
+}
